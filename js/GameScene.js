4b7a8b11ee146af05beed904b0cab27c9ab400e3
Nuss <mmh.nuss@gmail.com>
2024-05-17 23:05:13 +0800

    appearance of star

--- js/GameScene.js
+++ js/GameScene.js
@@ -19,7 +19,7 @@ class GameScene extends Phaser.Scene {
 
         // 创建光标键输入
         this.cursors = this.input.keyboard.createCursorKeys();
-        this.particles = this.add.particles('white');
+        // this.particles = this.add.particles('white');
         
 
         // this.tweens.add({
@@ -49,19 +49,41 @@ class GameScene extends Phaser.Scene {
         });
 
         this.physics.add.overlap(this.circle, this.stars, this.collectStar, null, this);
+
         
     }
 
     spawnStar() {
         const x = Phaser.Math.Between(0, 800);
         const y = Phaser.Math.Between(0, 600);
-        const star = this.stars.create(x, y, 'star');
-        star.setBounce(Phaser.Math.FloatBetween(0.4, 0.8));
-        star.setCollideWorldBounds(true);
+        // const star = this.stars.create(x, y, 'star');
+        // star.setBounce(Phaser.Math.FloatBetween(0.4, 0.8));
+        // star.setCollideWorldBounds(true);
+        const star = this.add.circle(x, y, 5, 0x000000);
+        this.physics.add.existing(star);
+        star.body.setBounce(Phaser.Math.FloatBetween(0.4, 0.8));
+        star.body.setCollideWorldBounds(true);
+        this.stars.add(star);
+
+        const starParticle = this.add.image(x, y, 'red').setAlpha(0.5).setScale(0.2);;
+        star.particles = [starParticle];
+
+        // const starParticles = this.add.particles('red');
+        // const starEmitter = starParticles.createEmitter({
+        //     speed: 50,
+        //     scale: { start: 0.2, end: 0 },
+        //     blendMode: 'ADD'
+        // });
+        // starEmitter.startFollow(star);
     }
 
     collectStar(circle, star) {
-        star.disableBody(true, true);
+        star.body.enable = false; // 禁用物理体
+        star.setVisible(false);   // 使星星不可见
+        star.setActive(false);    // 使星星失活
+        if (star.particles) {
+            star.particles.forEach(particle => particle.destroy()); // 销毁粒子
+        }
     }
 
     update() {
@@ -88,7 +110,13 @@ class GameScene extends Phaser.Scene {
             if (distance < 80) { // 距离小于200时，星星向黑球移动
                 this.physics.moveToObject(star, this.circle, 100);
             } else {
-                star.setVelocity(0);
+                star.body.setVelocity(0);
+            }
+            if (star.particles) {
+                star.particles.forEach(particle => {
+                    particle.x = star.x;
+                    particle.y = star.y;
+                });
             }
         });
     }
